73bfd1c82cc594f59081da28a1527b8c5522b1a8
Cyril Dupuis <dupuis.cyril@gmail.com>
2013-01-31 10:47:34 +0100

    Ajout de la page de stats (sans sammy) + refacto viewmodel.js

--- SammyAndSpa/Scripts/app/todo.viewmodel.js
+++ SammyAndSpa/Scripts/app/todo.viewmodel.js
@@ -1,43 +1,56 @@
 ﻿window.todoApp.todoListViewModel = (function (ko, datacontext) {
-    var todoLists = ko.observableArray(),
-        error = ko.observable(),
-        addTodoList = function () {
-            var todoList = datacontext.createTodoList();
-            todoList.IsEditingListTitle(true);
-            datacontext.saveNewTodoList(todoList)
-                .then(addSucceeded)
-                .fail(addFailed);
+    var self = this;
 
-            function addSucceeded() {
-                showTodoList(todoList);
-            }
-            function addFailed() {
-                error("Save of new TodoList failed");
-            }
-        },
-        showTodoList = function (todoList) {
-            todoLists.unshift(todoList); // Insert new TodoList at the front
-        },
-        deleteTodoList = function (todoList) {
-            todoLists.remove(todoList);
-            datacontext.deleteTodoList(todoList)
-                .fail(deleteFailed);
+    self.todoLists = ko.observableArray();
+    self.error = ko.observable();
+    self.addTodoList = function() {
+        var todoList = datacontext.createTodoList();
+        todoList.IsEditingListTitle(true);
+        datacontext.saveNewTodoList(todoList)
+            .then(addSucceeded)
+            .fail(addFailed);
 
-            function deleteFailed() {
-                showTodoList(todoList); // re-show the restored list
-            }
-        };
+        function addSucceeded() {
+            showTodoList(todoList);
+        }
+
+        function addFailed() {
+            error("Save of new TodoList failed");
+        }
+    };
+    
+    self.showTodoList = function (todoList) {
+        todoLists.unshift(todoList); // Insert new TodoList at the front
+    },
+
+    self.deleteTodoList = function (todoList) {
+        todoLists.remove(todoList);
+        datacontext.deleteTodoList(todoList)
+            .fail(deleteFailed);
+
+        function deleteFailed() {
+            showTodoList(todoList); // re-show the restored list
+        }
+    };
 
     datacontext.getTodoLists(todoLists, error); // load TodoLists
+    
+    self.todoListsCount = ko.computed(function () {
+        return todoLists().length;
+    }, this);
 
-    return {
-        todoLists: todoLists,
-        error: error,
-        addTodoList: addTodoList,
-        deleteTodoList: deleteTodoList
+    self.isStatsChoose = ko.observable(true);
+
+    self.goToStats = function() {
+        self.isStatsChoose(true);
+    };
+
+    self.goToTodos = function() {
+        self.isStatsChoose(false);
     };
 
 })(ko, todoApp.datacontext);
+
 
 // Initiate the Knockout bindings
 ko.applyBindings(window.todoApp.todoListViewModel);
